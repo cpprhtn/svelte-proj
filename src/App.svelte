fefaf8e799e66326cf9b6cc423bebb296e5bd4cf
cpprhtn <cpprhtn@naver.com>
2022-08-10 16:42:00 +0900

    add: if문

--- src/App.svelte
+++ src/App.svelte
@@ -40,6 +40,13 @@
 		job: 'AI Developer',
 		website: 'https://cpprhtn.duckdns.org'
 	}
+
+	// 조건문
+	let x = 0;
+
+	function handleAddX () {
+		x += 1;
+	}
 	</script>
 
 <main>
@@ -64,6 +71,18 @@
 <Child 
 	{...info}
 />
+
+<hr>
+
+<button on:click={handleAddX}>Click</button>
+
+{#if x > 10}
+	<p>{x}는 10보다 큽니다.</p>
+{:else if x > 5}
+	<p>{x}는 5보다 큽니다.</p>
+{:else}
+	<p>{x}는 5보다 작거나 같습니다.</p>
+{/if}
 
 <style>
 	main {
